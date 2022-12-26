2f29e260a3a5207102e3363baf4acf85aa18d9a6
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-12-26 19:55:25 +0600

    Login & blog page set up

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className="h-screen text-gray-400 text-6xl bg-gradient-to-l from-blue-900 via-slate-900 to-black flex justify-center items-center">
             This is Home Page...
+
+            C
+            <div className='w-12 h-12 border-8 border-dashed rounded-full animate-spin border-green-600'></div>
+            ming s
+            <div className='w-12 h-12 border-8 border-dashed rounded-full animate-spin border-green-600'></div>
+            <div className='w-12 h-12 border-8 border-dashed rounded-full animate-spin border-green-600'></div>
+            n...
         </div>
     );
 };
